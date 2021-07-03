50eea71e0b2cfff0c9bd055c075800cd394bea01
chelinchan24 <me@chelinchan.com>
2021-07-04 00:16:52 +0800

    1.0.0

---
+++ emma-bars-slide-segmented-controls.js
@@ -0,0 +1,41 @@
+$(window).on('load', function () {
+    emma_slide_hover('容器', '選項', '標題', '滑塊', '啟用')
+})
+
+function emma_slide_hover(containerClass, itemClass, itemTextClass, slideClass, activeClass) {
+    var container = $('.' + containerClass)
+    var item = $('.' + itemClass)
+    var slide = container.children('.' + slideClass)
+
+    goDefault()
+
+    $(document).on('click', '.' + itemClass, function () {
+        item.removeClass(activeClass)
+        $(this).addClass(activeClass)
+    })
+
+    item.hover(
+        function (){
+            var target = $(this).children('.' + itemTextClass)
+            setSlide(target)
+        } , function() {
+            goDefault()
+        }
+    )
+
+    function goDefault() {
+        var target = container.children('.' + activeClass).children('.' + itemTextClass)
+        setSlide(target)
+    }
+
+    function setSlide(target) {
+        slide.css({
+            'height' : target.height(),
+            'width' : target.width(),
+            'left' : target.position().left + 'px',
+            'top' : target.position().top + 'px',
+        })
+    }
+}
+
+
